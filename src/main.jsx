39a9d97256ccc7e42ab430d09d3344c887e4fd20
atulispro1 <atul.sharmas2806@gmail.com>
2026-04-05 21:03:44 +0530

    alternativ err fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./context/ThemeContext";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </AuthProvider>,
+  <HelmetProvider>
+    <AuthProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AuthProvider>
+  </HelmetProvider>,
 );
